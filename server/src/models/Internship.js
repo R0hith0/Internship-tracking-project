295308dde418276ad56898d25425a1cde7e16ea0
Rohith Raj A <rohith27687@gmail.com>
2026-07-03 07:25:21 +0000

    Added Internship model and working POST API with MongoDB integration

---
+++ server/src/models/Internship.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const internshipSchema = new mongoose.Schema({
+  companyName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  role: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  qualification: {
+    type: String,
+    required: true
+  },
+
+  experienceRequired: {
+    type: String,
+    default: "Fresher"
+  },
+
+  stipend: {
+    type: String
+  },
+
+  workMode: {
+    type: String,
+    enum: ["REMOTE", "HYBRID", "ONSITE"],
+    default: "REMOTE"
+  },
+
+  duration: {
+    type: String
+  },
+
+  appliedOn: {
+    type: Date,
+    default: Date.now
+  },
+
+  deadline: {
+    type: Date
+  },
+
+  workingHours: {
+    type: String
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Internship", internshipSchema);
--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,4 @@
+const Internship = require("./models/Internship");
 require("dotenv").config();
 
 
@@ -12,6 +13,22 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Server is running :D ");
+});
+
+app.post("/internships", async (req, res) => {
+    try {
+        const internship = await Internship.create(req.body);
+
+        res.status(201).json({
+            message: "Internship created successfully",
+            data: internship
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating internship",
+            error: error.message
+        });
+    }
 });
 
 app.listen(3000, () => {
